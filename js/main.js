9305e99dfef85e4faa5ac4e466a2c23cdf5ca7b2
andrew-winchell <108092629+andrew-winchell@users.noreply.github.com>
2022-08-02 16:43:20 -0500

    Add Web Scene Base

--- js/main.js
+++ js/main.js
@@ -4,8 +4,11 @@ require([
     "esri/identity/IdentityManager",
     "esri/Map",
     "esri/views/SceneView",
-    "esri/layers/FeatureLayer"
-], function (promiseUtils, OAuthInfo, esriID, Map, SceneView, FeatureLayer) {
+    "esri/layers/FeatureLayer",
+    "esri/layers/TileLayer",
+    "esri/layers/ElevationLayer",
+    "esri/Basemap"
+], function (promiseUtils, OAuthInfo, esriID, Map, SceneView, FeatureLayer, TileLayer, ElevationLayer, Basemap) {
 
     //OAuth certification process
     //Required to access secure content from AGOL
@@ -22,4 +25,31 @@ require([
         .then(() => {
             console.log("Sign in completed.")
         });
+
+    //world topo basemap layer
+    const basemap = new Basemap({
+        baseLayers: [
+            new TileLayer({
+                url: "https://wtb.maptiles.arcgis.com/arcgis/rest/services/World_Topo_Base/MapServer"
+            })
+        ]
+    });
+
+    const map = new Map({
+        ground: {
+            layers: [
+                new ElevationLayer({
+                    url: "https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer"
+                })
+            ]
+        },
+        basemap: basemap
+    });
+
+    const scene = new SceneView({
+        container: "sceneView",
+        map: map,
+        center: [-98.5795, 39.8283],
+        zoom: 5
+    })
 })
