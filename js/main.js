efbc3d5bcec72cbbc59a1697d05a408d024ba628
andrew-winchell <108092629+andrew-winchell@users.noreply.github.com>
2022-08-12 13:17:41 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -132,7 +132,7 @@ require([
             flights.definitionExpression = 
                 'MILLISECONDS <= ' + (timeSlider.timeExtent.end.getTime() + 30000)
                 + ' AND MILLISECONDS >= ' + (timeSlider.timeExtent.end.getTime() - 30000)
-                + ' AND ID = AAL1000';
+                + ' AND ID = "AAL1000"';
         })
     })
 })
